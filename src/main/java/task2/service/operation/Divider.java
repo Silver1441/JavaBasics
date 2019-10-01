3c9331777c00d1d1db8fe1d32f7f311fa318d692
Ivan <Kishkan91@gmail.com>
2019-10-01 22:00:54 +0400

    task2: divided by zero is added to exception

--- src/main/java/task2/service/operation/Divider.java
+++ src/main/java/task2/service/operation/Divider.java
@@ -1,7 +1,12 @@
 package task2.service.operation;
 
+import task2.exception.WrongInputException;
+
 public class Divider {
-    public static double divideTwoExpressions(double denominator, double numerator) {
+    public static double divideTwoExpressions(double denominator, double numerator) throws WrongInputException {
+        if (denominator == 0){
+            throw new WrongInputException("divided by zero");
+        }
         return numerator / denominator;
 
     }
