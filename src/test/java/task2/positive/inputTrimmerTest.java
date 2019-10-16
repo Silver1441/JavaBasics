bbf94e6744b2cc304e2fe170ed5e872396360c4b
Ivan <Kishkan91@gmail.com>
2019-10-16 12:03:05 +0400

    Task2 minor refactoring inputTrimmerTest

--- src/test/java/task2/positive/inputTrimmerTest.java
+++ src/test/java/task2/positive/inputTrimmerTest.java
@@ -1,6 +1,8 @@
 package task2.positive;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import task2.support.InputTrimmer;
 
 import static org.hamcrest.CoreMatchers.not;
@@ -9,17 +11,24 @@ import static org.hamcrest.Matchers.containsString;
 
 
 class inputTrimmerTest {
+    private String string = "[ [((   ,,..   ))] ]";
 
-    @Test
-    public void testTrimTheInput() {
-        String string = "[ [((   ,,..   ))] ]";
+    @ParameterizedTest
+    @CsvSource({"]", "["})
+    public void testTrimTheInput(String wrongInput) {
         String result = InputTrimmer.trimTheInput(string);
-        assertThat(result, not(containsString(" ")));
-        assertThat(result, not(containsString("]")));
-        assertThat(result, not(containsString("[")));
-        assertThat(result, not(containsString(",")));
+        assertThat(result, not(containsString(wrongInput)));
     }
 
+    @Test
+    public void testTrimTheInputEmptySpace() {
+        String result = InputTrimmer.trimTheInput(string);
+        assertThat(result, not(containsString(" ")));
+    }
 
-
+    @Test
+    public void testTrimTheInputComma() {
+        String result = InputTrimmer.trimTheInput(string);
+        assertThat(result, not(containsString(",")));
+    }
 }
